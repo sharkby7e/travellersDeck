b4a4904955e4e356b97dde5305b62c47676e5a7b
sharkbyte <sgquin@gmail.com>
2022-05-04 11:08:17 -0700

    base files added, models done

---
+++ models/Trip.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Trip extends Model {}
+
+Trip.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    trip_budget: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    traveller_amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "driver",
+  }
+);
+
+module.exports = Trip;
